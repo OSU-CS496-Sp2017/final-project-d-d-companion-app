1c8fb57901a83a6087e1746fb2d6ab1ea505be3e
Sanlador <cunardr@oregonstate.edu>
2017-06-10 18:53:22 -0700

    Minor Update
    Minor organizational changes to the character builder activity. Also set
    up the app to have a proper main activity that can access other
    activities, so that team members can work on different parts of the app.

--- DnDCompanionApp/app/src/main/java/cs496/dndcompanionapp/MainActivity.java
+++ DnDCompanionApp/app/src/main/java/cs496/dndcompanionapp/MainActivity.java
@@ -1,68 +1,33 @@
 package cs496.dndcompanionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
     TabHost tabHost;
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost host = (TabHost)findViewById(R.id.characterTabHost);
-        host.setup();
+        Button charBuilder = (Button)findViewById(R.id.goToChar);
 
-
-        //allignment layout
-        Spinner alignmentSpinner = (Spinner) findViewById(R.id.alignmentSpinner);
-        ArrayAdapter<CharSequence> alignmentAdapter = ArrayAdapter.createFromResource(this, R.array.alignmentArray, android.R.layout.simple_spinner_item);
-        alignmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        alignmentSpinner.setAdapter(alignmentAdapter);
-
-        //background layout
-        Spinner backgroundSpinner = (Spinner) findViewById(R.id.backgroundSpinner);
-        ArrayAdapter<CharSequence> backgroundAdapter = ArrayAdapter.createFromResource(this, R.array.backgroundPlaceholder, android.R.layout.simple_spinner_item);
-        backgroundAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        backgroundSpinner.setAdapter(backgroundAdapter);
-
-        //
-
-        //Background tab
-        TabHost.TabSpec spec = host.newTabSpec("Background");
-        spec.setContent(R.id.background);
-        spec.setIndicator("Background");
-        host.addTab(spec);
-
-        //Race and Class tab
-        spec = host.newTabSpec("Race and Class");
-        spec.setContent(R.id.RaceClass);
-        spec.setIndicator("Race and Class");
-        host.addTab(spec);
-
-        //Stats tab
-        spec = host.newTabSpec("Stats");
-        spec.setContent(R.id.Stats);
-        spec.setIndicator("Stats");
-        host.addTab(spec);
-
-        //Items tab
-        spec = host.newTabSpec("Items");
-        spec.setContent(R.id.Items);
-        spec.setIndicator("Items");
-        host.addTab(spec);
-
-        //Feats and Spells tab
-        spec = host.newTabSpec("Feats and Spells");
-        spec.setContent(R.id.FeatsAndSpells);
-        spec.setIndicator("Feats and Spells");
-        host.addTab(spec);
+        charBuilder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CharacterBuilderActivity.class));
+            }
+        });
 
 
     }
 }
+
