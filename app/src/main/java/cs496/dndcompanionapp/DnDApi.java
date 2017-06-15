340207ae9cd3c43fc875c6b61693d186d6181618
ginafish <phippsg@oregonstate.edu>
2017-06-14 17:46:39 -0700

    overloaded buildSearchUrl to have a method just for querying the root

--- app/src/main/java/cs496/dndcompanionapp/DnDApi.java
+++ app/src/main/java/cs496/dndcompanionapp/DnDApi.java
@@ -1,4 +1,8 @@
+package cs496.dndcompanionapp;
+
 import android.net.Uri;
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,7 +16,59 @@ public class DnDApi {
 	
 	private final static String DND_SEARCH_BASE_URL = "http://dnd5eapi.co/api/";
 	
-	
+
+	public static String buildSearchURL (String searchType) {
+		String url = DND_SEARCH_BASE_URL;
+
+		switch(searchType) {
+			case "class": case "classes":
+				url = url + "classes/";
+				break;
+
+			case "subclass": case "subclasses":
+				url = url + "subclasses/";
+				break;
+
+			case "race": case "races":
+				url = url + "races/";
+				break;
+
+			case "subrace": case "subraces":
+				url = url + "subraces/";
+				break;
+
+			case "skills":
+				url = url + "skills/";
+				break;
+
+			case "spell": case "spells":
+				url = url + "spells/";
+				//note: if looking for specific spell, must be an index.
+				break;
+
+			case "proficiencies":
+				url = url + "proficiencies/";
+				//must cast as lowercase
+				//can search by class, index, or keyword (ie: armor, weapons)
+				break;
+
+			case "languages":
+				url = url + "languages/";
+				break;
+
+			case "equipment":
+				url = url + "equipment/";
+				break;
+
+			default:
+				Log.d(TAG, "DnDApi.buildSearchURL: invalid searchType.");
+				break;
+		}
+		Log.d(TAG, "API root query url: " + url);
+		return url;
+	}
+
+
 	public static String buildSearchURL (String query, String searchType) {
 		String url = DND_SEARCH_BASE_URL;
 		
@@ -56,9 +112,13 @@ public class DnDApi {
 				url = url + "equipment/";
 				break;
 			
-			case default:
+			default:
 				Log.d(TAG, "DnDApi.buildSearchURL: invalid searchType.");
 				break;
 		}
+
+		url = url + query;
+		Log.d(TAG, "URL with query: " + url);
+		return url;
 	}
 }
