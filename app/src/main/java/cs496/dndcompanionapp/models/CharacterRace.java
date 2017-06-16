5139c84d4d063690a9baa107640600d617dc90f5
ginafish <phippsg@oregonstate.edu>
2017-06-16 11:32:03 -0700

    Started raceDetail activity

---
+++ app/src/main/java/cs496/dndcompanionapp/models/CharacterRace.java
@@ -0,0 +1,15 @@
+package cs496.dndcompanionapp.models;
+
+/**
+ * Created by G on 6/16/2017.
+ */
+
+public class CharacterRace {
+    public String _id;
+    public String index;
+    public String name;
+    public String alignment;
+    public String age;
+    public String size;
+    public String sizeDescription;
+}
