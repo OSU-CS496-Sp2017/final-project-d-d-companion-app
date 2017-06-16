32d2d015561586e1c85d55ef38346f25f9649847
ginafish <phippsg@oregonstate.edu>
2017-06-16 00:24:04 -0700

    Finished the layout

--- app/src/main/java/cs496/dndcompanionapp/SearchManualAdapter.java
+++ app/src/main/java/cs496/dndcompanionapp/SearchManualAdapter.java
@@ -1,10 +1,18 @@
 package cs496.dndcompanionapp;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by G on 6/15/2017.
  */
 
-public class SearchManualAdapter {
+public class SearchManualAdapter extends AppCompatActivity {
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 }
