0d1490d0d4d45c3de8430a8c0f25a9bba2b0a110
Vorhut <thenellb@oregonstate.edu>
2017-06-15 21:02:43 -0700

    Added first theme and all related functions

--- app/src/main/java/cs496/dndcompanionapp/MainActivity.java
+++ app/src/main/java/cs496/dndcompanionapp/MainActivity.java
@@ -3,7 +3,9 @@ package cs496.dndcompanionapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //setTheme(R.style.Barbarian);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String theme =
+                sharedPreferences.getString(
+                        getString(R.string.theme_key),
+                        getString(R.string.theme_default)
+                );
+        changeTheme(theme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -52,5 +62,64 @@ public class MainActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        //run();
+
+    }
+    public void run() {
+        Intent intent =getIntent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+       overridePendingTransition(0, 0);
+      finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+    }
+    public void changeTheme(String theme){
+        Log.d("TAG","Value of units in changeTheme:  " + theme);
+        switch (theme) {
+            case "Bard":
+                setTheme(R.style.Bard);
+                break;
+            case "Barbarian":
+                setTheme(R.style.Barbarian);
+                break;
+            case "Cleric":
+                //setTheme();
+                break;
+            case "Druid":
+                //setTheme();
+                break;
+            case "Fighter":
+                //setTheme();
+                break;
+            case "Monk":
+                //setTheme();
+                break;
+            case "Paladin":
+                //setTheme();
+                break;
+            case "Ranger":
+                //setTheme();
+                break;
+            case "Rogue":
+                //setTheme();
+                break;
+            case "Sorcerer":
+                //setTheme();
+                break;
+            case "Warlock":
+                //setTheme();
+                break;
+            case "Wizard":
+                //setTheme();
+                break;
+            default:
+                break;
+        }
+    }
 }
+
 
--- app/src/main/java/cs496/dndcompanionapp/SettingsActivity.java
+++ app/src/main/java/cs496/dndcompanionapp/SettingsActivity.java
@@ -2,16 +2,72 @@ package cs496.dndcompanionapp;
 
 
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String theme =
+                sharedPreferences.getString(
+                        getString(R.string.theme_key),
+                        getString(R.string.theme_default)
+                );
+        changeTheme(theme);
+        Log.d("TAG","Value of units:  "+ theme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+    public void changeTheme(String theme){
+        Log.d("TAG","Value of units in changeTheme:  " + theme);
+        switch (theme) {
+            case "Bard":
+                setTheme(R.style.Bard);
+
+                break;
+            case "Barbarian":
+                setTheme(R.style.Barbarian);
+                break;
+            case "Cleric":
+                //setTheme();
+                break;
+            case "Druid":
+                //setTheme();
+                break;
+            case "Fighter":
+                //setTheme();
+                break;
+            case "Monk":
+                //setTheme();
+                break;
+            case "Paladin":
+                //setTheme();
+                break;
+            case "Ranger":
+                //setTheme();
+                break;
+            case "Rogue":
+                //setTheme();
+                break;
+            case "Sorcerer":
+                //setTheme();
+                break;
+            case "Warlock":
+                //setTheme();
+                break;
+            case "Wizard":
+                //setTheme();
+                break;
+            default:
+                break;
+        }
     }
 
 
